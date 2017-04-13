7e3b8b53cb1c050326482c18954c8669cf9bd733
Kamzoki <kaittah@ymail.com>
2017-04-13 14:55:48 +0200

    top down parser, have a problem with toknize

--- Arabization implementation.py
+++ Arabization implementation.py
@@ -1,17 +1,23 @@
 from __future__ import unicode_literals
 from nltk import CFG
+from nltk.parse import RecursiveDescentParser
 
-m_Grammer = CFG.fromstring("""
+m_Grammar = CFG.fromstring("""
 
-S -> VP NP
-S -> VP ADJP
+S -> VP NP|NP VP|VP ADJP|NP ADJP
 VP -> V
 NP -> NN ADJP|NN|NN NP| DetNN
 ADJP -> NN Adj|NP|NP Adj|DetNN DetAdj| NP DetAdj
- 
+
 V -> 'حضرت'| 'رأيت'|'فاز'
 NN ->'كاتبة'|'قصص'|'حصان'|'صاحب'|'مارد'
 Adj -> 'عملاق'|'وفي'|'رائع'|'رشيق'|'علاقية'
 DetAdj -> 'الرائعة'
 DetNN -> 'الحديقة'
 """)
+
+def fn_RDP(sent):
+    sent.split()
+    rd = RecursiveDescentParser (m_Grammar)
+    for w in rd.parse(sent):
+        print(w)
