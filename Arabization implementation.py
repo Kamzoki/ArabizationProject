10bbd2e413951411135441f62fbaba49b592ae02
Kamzoki <kaittah@ymail.com>
2017-04-13 14:30:59 +0200

    Grammar for testing

--- Arabization implementation.py
+++ Arabization implementation.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from nltk import CFG
 
 m_Grammer = CFG.fromstring("""
@@ -5,13 +6,12 @@ m_Grammer = CFG.fromstring("""
 S -> VP NP
 S -> VP ADJP
 VP -> V
-NP -> NN ADJP|Det+NN|NN|NN NP 
-ADJP -> NN Adj|NP Det+Adj|NP Adj|Det+NN Det+Adj
+NP -> NN ADJP|NN|NN NP| DetNN
+ADJP -> NN Adj|NP|NP Adj|DetNN DetAdj| NP DetAdj
  
 V -> 'حضرت'| 'رأيت'|'فاز'
 NN ->'كاتبة'|'قصص'|'حصان'|'صاحب'|'مارد'
 Adj -> 'عملاق'|'وفي'|'رائع'|'رشيق'|'علاقية'
-Det+Adj -> 'الرائعة'
-Det+NN -> 'الحديقة'
-
+DetAdj -> 'الرائعة'
+DetNN -> 'الحديقة'
 """)
